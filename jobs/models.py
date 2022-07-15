f8b43d2b1eb2a9ffdb2b8ee512f0e7271c511a85
npatil2022 <npatil@bink.com>
2022-07-15 13:38:30 +0100

    portfolio_project

--- jobs/models.py
+++ jobs/models.py
@@ -5,3 +5,15 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to='')
     summary = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.summary
+
+
+class Job_ac(models.Model):
+    image = models.ImageField(upload_to='')
+    summary = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.summary
+
